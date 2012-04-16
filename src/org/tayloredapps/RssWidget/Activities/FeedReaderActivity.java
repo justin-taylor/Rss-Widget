6148065bff87d3e21b17386ff239742b6ff1a6c8
Justin Taylor <j.taylor8727@gmail.com>
2012-04-15 23:11:03 -0400

    Added list of rss feeds

--- src/org/tayloredapps/RssWidget/Activities/FeedReaderActivity.java
+++ src/org/tayloredapps/RssWidget/Activities/FeedReaderActivity.java
@@ -1,17 +1,37 @@
 package org.tayloredapps.RssWidget.Activities;
 
 import org.tayloredapps.R;
-import org.tayloredapps.R.layout;
+import org.tayloredapps.RssWidget.Adapters.FeedAdapter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 public class FeedReaderActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	private FeedAdapter adapter = new FeedAdapter(this, 1);
+	
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ListView lv = (ListView) findViewById(R.id.listView1);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(lvListener);
     }
+    
+    private OnItemClickListener lvListener = new OnItemClickListener(){
+
+		public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+		{
+			//TODO get the article and pass to the next activity
+		}
+    };
+    
+    
 }
---
+++ src/org/tayloredapps/RssWidget/Adapters/FeedAdapter.java
@@ -0,0 +1,50 @@
+package org.tayloredapps.RssWidget.Adapters;
+
+import java.util.List;
+
+import org.tayloredapps.R;
+import org.tayloredapps.RssWidget.Models.RssFeed;
+
+import com.orm.androrm.Model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class FeedAdapter extends ArrayAdapter<RssFeed>
+{
+	List<RssFeed> feeds;
+	
+	public FeedAdapter(Context context, int textViewResourceId)
+	{
+		super(context, textViewResourceId);
+		feeds = RssFeed.getAllFeeds();
+	}
+
+	public int getCount()
+	{
+		return feeds.size();
+	}
+	
+	public RssFeed getItem(int position)
+	{
+		return feeds.get(position);
+	}
+	
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		if (convertView == null)
+		{	
+			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.row, parent, false);
+		}
+		
+		TextView textView = (TextView) convertView.findViewById(R.id.label);
+		RssFeed feed = feeds.get(position);
+		textView.setText(feed.getTitle());
+		return convertView;
+	}
+}
